1c38c718d1b1af4ddad92f157b64ea0690848737
jechoi <jechoi1111@gmail.com>
2024-01-03 21:09:58 +0900

    1002번 문제 - 다시 풀어보기

--- 1002.py
+++ 1002.py
@@ -3,19 +3,15 @@ import math
 testCase = int(input())
 
 for i in range(testCase):
-    value = input().split()
-    x1 = int(value[0])
-    x2 = int(value[1])
-    y1 = int(value[3])
-    y2 = int(value[4])
-    r1 = int(value[2])
-    r2 = int(value[5])
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
 
-    length = math.sqrt((x1-x2)**2 + (y1-y2)**2)
-    print(length, r1 + r2)
-    if length < r1 + r2:
-        print(2)
-    elif length == r1 + r2:
+    distance = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+    if distance == 0 and r1 == r2:
+        print(-1)
+    elif abs(r1 - r2) == distance or distance == (r1 + r2):
         print(1)
+    elif abs(r1 - r2) < distance < (r1 + r2):
+        print(2)
     else:
         print(0)
