2b865b27c6af81d252a743156720c6b48f9692b0
jechoi <jechoi1111@gmail.com>
2024-01-10 23:30:25 +0900

    1009번 문제 - 틀림

---
+++ 1009.py
@@ -0,0 +1,14 @@
+testCase = int(input())
+
+for i in range(int(testCase)):
+    a, b = map(int, input().split())
+    a = a % 10
+
+    if a == 0:
+        print(10)
+    elif a == 1 or a == 5 or a == 6:
+        print(a)
+    elif a == 4 or a == 9:
+        print((a ** (b % 2)) % 10)
+    else:
+        print((a ** (b % 4)) % 10)
