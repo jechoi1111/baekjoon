693584728f7d63fd8d9b547537323100b246c66c
jechoi <choejeong-eun@choejeong-eun-ui-MacBookPro.local>
2024-01-21 19:09:28 +0900

    9498번 문제

---
+++ 9498.py
@@ -0,0 +1,11 @@
+value = int(input())
+
+if 90 <= value <= 100:
+    print('A')
+elif 80 <= value <= 89:
+    print('B')
+elif 70 <= value <= 79:
+    print('C')
+elif 60 <= value <= 69:
+    print('D')
+else: print('F')
