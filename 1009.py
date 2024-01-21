186cde55ca32a5015dbae23d377a247dd32a65fa
jechoi <choejeong-eun@choejeong-eun-ui-MacBookPro.local>
2024-01-21 17:55:55 +0900

    1009번 문제 - 다시 풀어보기

--- 1009.py
+++ 1009.py
@@ -9,9 +9,12 @@ for i in range(int(testCase)):
     elif a == 1 or a == 5 or a == 6:
         print(a)
     elif a == 4 or a == 9:
-        print((a ** (b % 2)) % 10)
-    else:
-        print((a ** (b % 4)) % 10)
-
-
-# 맥북 테스트 커밋
+        if b % 2 == 0:
+            print((a ** 2) % 10)
+        else:
+            print(a)
+    else: # 2, 3, 7, 8
+        if b % 4 == 0:
+            print(a ** 4 % 10)
+        else:
+            print((a ** (b % 4)) % 10)
